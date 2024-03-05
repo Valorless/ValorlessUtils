89c8bdc2d4a81a861005c89c59927e90c9147f94
Valor <Minac2503@hotmail.dk>
2024-03-05 11:40:17 +0100

    update GUI.java

---
+++ src/main/java/valorless/valorlessutils/gui/GUI.java
@@ -0,0 +1,79 @@
+Public class GUI implements Listener {
+
+    Public enum GUIAction{PREV_PAGE, NEXT_PAGE,CONFIRM,CANCEL,RETURN,OPEN_PAGE, CLOSE,OPEN_INVENTORY,GIVE_ITEM,GIVE_MONEY}
+
+    Public class Row {
+        Public List<ItemStack> Content;
+        
+        Public Row(){}
+        
+        Public void SetItem(int index,ItemStack item){
+            index = Utils.Clamp(index,0,8);
+            Content.set(index,item)
+            
+        }
+        
+        Public void RemoveItem(int index){
+            index = Utils.Clamp(index,0,8);
+            Content.set(index,null);
+        }
+        
+        Public void SetContent(List<ItemStack> content){
+            this.Content > content;
+        }
+    }
+    
+    Public class Page {
+        
+        Public List<Row> rows;
+        
+        Public String title;
+        
+        Public Pahe(){}
+        
+        Public Page(List<Row> rows, String title){
+            this.rows = rows;
+            this.title = title;
+        }
+        
+        Public void SetRows(List<Row> rows){
+            this.rows = rows;
+        }
+        
+        Public void AddRow(Row row){
+            this.rows.add(row);
+        }
+        
+        Public void RemoveRow(int index){
+            this.rows.remove(index);
+        }
+        
+        Public void SetTitle(String title){
+            this.title = title;
+        }
+    }
+    
+    // Variables
+    
+    Public List<Listener> listeners;
+    
+    @EventHandler
+    Public void onInventoryClick(InventoryClickEvent event){
+        // more.
+        for (Listener listener : listeners){
+            try{
+                listener.onGUIEvent(GUIAction action, object... args);
+            }catch(Exception e){}
+        }
+    }
+    
+    Public void RegisterListener(Listener listener){
+        listeners.add(listener);
+    }
+    
+    Public void UnregisterListener(Listener listener){
+        listeners.remove(listener);
+    }
+    
+
+}
