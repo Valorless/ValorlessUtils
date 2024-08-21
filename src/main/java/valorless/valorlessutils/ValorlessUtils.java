a7fdfb4b87f3029edd99969d78fdb116ac25db28
Valorless <Minac2503@hotmail.dk>
2024-08-21 21:13:06 +0200

    Build 238
    Fixed: The following error:
    [ValorlessUtils] Failed to resolve server version, some functions might not work correctly.
    
    Fixed: Forgot to update language files.

--- src/main/java/valorless/valorlessutils/ValorlessUtils.java
+++ src/main/java/valorless/valorlessutils/ValorlessUtils.java
@@ -26,7 +26,7 @@ import valorless.valorlessutils.types.Vector;
 import valorless.valorlessutils.types.Vector3;
 
 public final class ValorlessUtils extends JavaPlugin implements Listener {
-	public enum Version { NULL, v1_17, v1_17_1, v1_18, v1_18_1, v1_18_2, v1_19, v1_19_1, v1_19_2, v1_19_3, v1_19_4, v1_20, v1_20_1, v1_20_3, v1_20_4, v1_20_5, v1_20_6, v1_21 }
+	public enum Version { NULL, v1_17, v1_17_1, v1_18, v1_18_1, v1_18_2, v1_19, v1_19_1, v1_19_2, v1_19_3, v1_19_4, v1_20, v1_20_1, v1_20_3, v1_20_4, v1_20_5, v1_20_6, v1_21, v1_21_1 }
     public static JavaPlugin thisPlugin;
     String Name = "§7[§6Valorless§bUtils§7]§r";
 	
